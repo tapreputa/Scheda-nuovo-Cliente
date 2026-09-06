1a0a1c76cb06bbad2c9852302d8e5303c18bab13
tapreputa <tapreputa@gmail.com>
2026-09-06 20:10:31 +0200

    Align preview refresh with stable5

--- tap-preview-refresh.js
+++ tap-preview-refresh.js
@@ -3,7 +3,7 @@
 
   if ((location.pathname.split('/').pop() || '') !== 'personalizza.html') return;
 
-  const BUILD_ID = '20260906-stable4';
+  const BUILD_ID = '20260906-stable5';
   const REOPEN_KEY = 'tap_preview_reopen_v3';
 
   function readLogoData() {
