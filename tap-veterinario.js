decc26917b503468871ba1da98d2292ccb80e97a
tapreputa <tapreputa@gmail.com>
2026-09-06 11:09:54 +0200

    Load Macelleria review fixes

--- tap-veterinario.js
+++ tap-veterinario.js
@@ -40,6 +40,7 @@
       await loadScript('tap-category-review-fixes.js');
       await loadScript('tap-category-mode-separation.js');
       await loadScript('tap-cartolibreria-review.js');
+      await loadScript('tap-macelleria-review.js');
     } catch (error) {
       console.error('Tapreputa: inizializzazione moduli Personalizza non riuscita.', error);
     }
