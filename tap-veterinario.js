475c00155688e8b84d949dea8dc4689c176e3426
tapreputa <tapreputa@gmail.com>
2026-09-16 01:51:41 +0200

    Bump Personalizza build for canonical processed logo

--- tap-veterinario.js
+++ tap-veterinario.js
@@ -3,7 +3,7 @@
 
   if ((location.pathname.split('/').pop() || '') !== 'personalizza.html') return;
 
-  const BUILD_ID = '20260916-stable5.6';
+  const BUILD_ID = '20260916-stable5.7';
 
   function syncAuthenticatedOperator() {
     const select = document.getElementById('operatorSelect');
